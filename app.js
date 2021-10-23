854563495bf13bea47e2e7c4a27bf9f4bd2c6918
chandanrajbongshi99 <chandantangla@gmail.com>
2021-10-24 01:04:19 +0530

    intial commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser=require('body-parser');
 const app=express();
-
+const PORT=process.env.PORT || 3000;
 app.set('view engine',"ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -12,6 +12,6 @@ app.get('/home', function(req, res) {
 
 
 
-  app.listen(3000, function() {
+  app.listen(PORT, function() {
     console.log("Server started on port 3000");
   });
